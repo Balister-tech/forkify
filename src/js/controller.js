47b2fb9e4ee67875b5a8c5e5fb668b8a330cb2b1
Balister-tech <balisterkum23@gmail.com>
2021-01-02 18:34:10 +0530

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,9 +133,7 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function() {
-  console.log('Welcome to the application');
-}
+
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -145,7 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
